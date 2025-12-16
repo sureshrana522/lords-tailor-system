73550dc7cc9b240d8fddea456ac50b993b5ebedc
sureshrana522 <sureshranasureshrana522@gmail.com>
2025-12-16 14:41:15 +0530

    Update App.tsxFix App.tsx syntax

--- App.tsx
+++ App.tsx
@@ -1,9 +1,8 @@
+import AdminDashboard from "./components/AdminDashboard";
+
 function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Lords Tailor System</h1>
-      <p>Deployment working</p>
-    </div>
+    <AdminDashboard />
   );
 }
 
